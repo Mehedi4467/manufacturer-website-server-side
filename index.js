7bb2b71dc5c2ae0ca39392ce2900d131e50516ab
Mehedi4467 <mehedi446768@gmail.com>
2022-05-24 05:26:46 +0600

    products api created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const ObjectId = require('mongodb').ObjectId;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -23,8 +24,28 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const collection = client.db("Auto_care").collection("Products");
-        console.log("hello ")
+        const productsCollection = client.db("Auto_care").collection("Products");
+
+
+        // product api
+        // get all product api
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+        // get products by id 
+
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.findOne(query);
+            res.send(result);
+
+        });
 
     }
     finally {
